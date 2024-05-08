2fb13314ffdef4087e6211e8c881c9c74860a9ae
John Walker <41120773+johnmwalker@users.noreply.github.com>
2024-05-07 22:06:46 -0500

    Update template_startup.sh

--- template_startup.sh
+++ template_startup.sh
@@ -10,6 +10,15 @@ cd /
 sudo apt-get update
 sudo apt-get install -y git
 
+# Install gh
+(type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
+&& sudo mkdir -p -m 755 /etc/apt/keyrings \
+&& wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+&& sudo apt update \
+&& sudo apt install gh -y
+
 # Set up Git
 git clone https://user:pat@github.com/johnmwalker/cloudheim.git
 git config --global --add safe.directory /cloudheim
